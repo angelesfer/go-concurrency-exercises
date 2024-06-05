a75b9534720060799dd17e97af0e611157036328
Angel Esquinas <a.esquinas.fernandez@gmail.com>
2024-06-05 12:18:58 +0200

    Exercise 1 done

--- 1-producer-consumer/main.go
+++ 1-producer-consumer/main.go
@@ -10,22 +10,38 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func producer(stream Stream) (tweets []*Tweet) {
-	for {
-		tweet, err := stream.Next()
-		if err == ErrEOF {
-			return tweets
-		}
+func producer(stream Stream, wg *sync.WaitGroup) <-chan *Tweet {
 
-		tweets = append(tweets, tweet)
-	}
+	tweetChan := make(chan *Tweet)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		for {
+			tweet, err := stream.Next()
+			if err == ErrEOF {
+				close(tweetChan)
+				return
+			} else {
+				tweetChan <- tweet
+			}
+		}
+	}()
+	return tweetChan
 }
 
-func consumer(tweets []*Tweet) {
-	for _, t := range tweets {
+func consumer(tweetChan <-chan *Tweet, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		t, more := <-tweetChan
+		if !more {
+			return
+		}
 		if t.IsTalkingAboutGo() {
 			fmt.Println(t.Username, "\ttweets about golang")
 		} else {
@@ -35,14 +51,18 @@ func consumer(tweets []*Tweet) {
 }
 
 func main() {
+	var wg sync.WaitGroup
 	start := time.Now()
 	stream := GetMockStream()
 
 	// Producer
-	tweets := producer(stream)
+	tweetChan := producer(stream, &wg)
 
 	// Consumer
-	consumer(tweets)
+	wg.Add(1)
+	go consumer(tweetChan, &wg)
+
+	wg.Wait()
 
 	fmt.Printf("Process took %s\n", time.Since(start))
 }
