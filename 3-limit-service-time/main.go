c57e1f65ab03f346c40457f7d8752a77c5cf69fd
Angel Esquinas <a.esquinas.fernandez@gmail.com>
2024-06-05 17:58:32 +0200

    Solution of exercise 3

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,12 @@
 
 package main
 
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -21,8 +27,35 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	// Set a timer to process
+	wt := make(chan int64)
+
+	limitContext, cancel := context.WithCancel(context.Background())
+
+	if !u.IsPremium {
+		limitContext, cancel = context.WithTimeout(context.Background(), (10*time.Second)-time.Duration(u.TimeUsed))
+	}
+
+	// Execute a controlled goroutine
+	defer cancel()
+
+	go func(ctx context.Context, wastedTime chan<- int64) {
+		defer ctx.Done()
+		start := time.Now()
+		process()
+		end := time.Now()
+		wastedTime <- end.Unix() - start.Unix()
+	}(limitContext, wt)
+	select {
+
+	case elapsed := <-wt:
+		fmt.Println("Consumed:", elapsed)
+		u.TimeUsed += elapsed
+		return true
+	case <-limitContext.Done():
+		fmt.Println("Error:", limitContext.Err())
+		return false
+	}
 }
 
 func main() {
